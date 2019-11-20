23ad1005506bcd78b5a92760eb1318b78a639995
jonathan <jonsr@mit.edu>
2019-11-20 17:22:37 -0500

    roberta scaling script tested

--- scripts/run_wt103_roberta_scaled.sh
+++ scripts/run_wt103_roberta_scaled.sh
@@ -9,11 +9,11 @@ function train_scaled(){
     encoder_attention_heads=$(python -c "print($width * 1)")
 
     dataset=wt103
-    datadir=$(python -c "print('../lm-data/wikitext-103-'+str($dsdiv)+'/')")
+    datadir=$(python -c "print('data-bin/wikitext-103-'+str($dsdiv)+'/')")
     ds_fraction=$(python -c "print(1/$dsdiv)")
     savepath=$(python -c "print('./transresults/width_'+str($width)+'_dsfraction_'+str($ds_fraction))")
 
-    CUDA_VISIBLE_DEVICES=[0,1,2,3,4,5,6,7]
+    CUDA_VISIBLE_DEVICES=[6,7]
     echo 'Run training... '
     echo $dataset
     echo $datadir
@@ -31,22 +31,28 @@ function train_scaled(){
     MAX_SENTENCES=16        # Number of sequences per batch (batch size)
     UPDATE_FREQ=16         # Increase the 
 
-    fairseq-train --fp16 $DATA_DIR \
+
+    # --arch roberta_1_64_256_1 \
+    fairseq-train --fp16 $datadir \
         --task masked_lm \
         --criterion masked_lm \
-        --arch roberta_1_64_256_1 \
+        --arch roberta \
+        --encoder-layers $encoder_layers \
+        --encoder-embed-dim $encoder_embed_dim \
+        --encoder-ffn-embed-dim $encoder_ffn_embed_dim \
+        --encoder-attention-heads $encoder_attention_heads \
         --sample-break-mode complete \
         --tokens-per-sample $TOKENS_PER_SAMPLE \
-        --optimizer adam --adam-betas '(0.9,0.98)' ֿ--adam-eps 1e-6 --clip-norm 0.0 \
+        --optimizer adam --adam-betas '(0.9,0.98)' --adam-eps 1e-6 --clip-norm 0.0 \
         --lr-scheduler polynomial_decay \
-        --lr $PEAK_LR \--warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
+        --lr $PEAK_LR --warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
         --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
         --max-sentences $MAX_SENTENCES --update-freq $UPDATE_FREQ \
         --max-update $TOTAL_UPDATES --log-format simple --log-interval 1
 
 }
-for w in 1 2 4
+for w in 1 
 do
-    train_scaled 128 $w
+    train_scaled 32 $w
 done
 
